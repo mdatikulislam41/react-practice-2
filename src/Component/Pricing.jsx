fc5285adc4df4fbf02fd28c604cbbcede537563d
mdatikulislam41 <mdatikulislam41@gmail.com>
2025-12-15 11:57:25 +0600

    Before Chart

--- src/Component/Pricing.jsx
+++ src/Component/Pricing.jsx
@@ -1,6 +1,13 @@
 import React, { use } from "react";
-
+import { CheckCheck } from "lucide-react";
 const Pricing = ({ pricingPromise }) => {
+  const buttonClassByPlan = {
+    basic: "bg-gray-200 text-gray-500 hover:bg-gray-300 active:bg-gray-400",
+    pro: "bg-indigo-500 text-white hover:bg-indigo-600 active:bg-indigo-700",
+
+    enterprise: "bg-gray-800 text-white hover:bg-gray-700 active:bg-black",
+  };
+
   const pricingData = use(pricingPromise);
 
   return (
@@ -14,12 +21,18 @@ const Pricing = ({ pricingPromise }) => {
             {pricingData.map((item, index, array) => (
               <div
                 key={index}
-                className="flex flex-col overflow-hidden rounded-lg border-2 border-indigo-500"
+                className={
+                  `flex flex-col overflow-hidden rounded-lg border-2  ` +
+                  (item.id !== "pro" ? "sm:mt-8 border" : "border-indigo-500")
+                }
               >
-                <div className="bg-indigo-500 py-2 text-center text-sm font-semibold uppercase tracking-widest text-white">
-                  Popular choise
-                </div>
-
+                {item.id == "pro" && (
+                  <div className="bg-indigo-500 py-2 text-center text-sm font-semibold uppercase tracking-widest text-white">
+                    Popular choise
+                  </div>
+                )}
+                {item.id == "basic" && <div class="h-2 bg-pink-500"></div>}
+                {item.id == "enterprise" && <div class="h-2 bg-gray-800"></div>}
                 <div className="flex flex-1 flex-col p-6 pt-8">
                   <div className="mb-12">
                     <div className="mb-2 text-center text-2xl font-bold text-gray-800">
@@ -34,27 +47,7 @@ const Pricing = ({ pricingPromise }) => {
                       {item.features.map((feature, index) => {
                         return (
                           <div key={index} className="flex items-center gap-2">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-4 w-4 shrink-0"
-                              fill="none"
-                              viewBox="0 0 16 16"
-                            >
-                              <circle
-                                cx="8"
-                                cy="8"
-                                r="8"
-                                fill="currentColor"
-                                className="text-gray-300"
-                              />
-                              <circle
-                                cx="8"
-                                cy="8"
-                                r="3"
-                                fill="currentColor"
-                                className="text-gray-500"
-                              />
-                            </svg>
+                            <span className="text-black">{<CheckCheck />}</span>
 
                             <span className="text-gray-600">{feature}</span>
                           </div>
@@ -66,9 +59,11 @@ const Pricing = ({ pricingPromise }) => {
                   <div className="mt-auto">
                     <a
                       href="#"
-                      className="block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+                      className={`block rounded-lg  px-8 py-3 text-center text-sm font-semibold  outline-none ring-indigo-300 transition duration-100  focus-visible:ring  md:text-base ${
+                        buttonClassByPlan[item.id]
+                      }`}
                     >
-                      $19
+                      ${item.price.amount}
                     </a>
                   </div>
                 </div>
